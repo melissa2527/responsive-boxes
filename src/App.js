942ba6920b935f7b05e71a561285b5d54f125115
annie2527 <you@example.com>
2021-02-24 12:26:34 +0100

    responsive boxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='box-container'>
+        <section style={{background: 'red'}}></section>
+        <section style={{background: 'green'}}></section>
+        <section style={{background: 'blue'}}></section>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// wrote in App to separate code
